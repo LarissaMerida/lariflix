3519d99a790dbc278c0801bf78828430a0185515
LarissaMerida <larissadasilvasantos746@gmail.com>
2020-07-28 20:53:07 -0300

    adicionado carrossel e form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,7 +6,15 @@ function CadastroCategoria(){
     return (
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
+        
+        <form>
+          <label>
+            Nome da Categoria:
+            <input ype="text"/>
+          </label>
 
+          <button>Cadastrar</button>
+        </form>
         
         <Link to="/">
             Ir para home
