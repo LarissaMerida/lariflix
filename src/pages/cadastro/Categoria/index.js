fe899b1c4011fd6b088bc62d8049d7893e049cda
LarissaMerida <larissadasilvasantos746@gmail.com>
2020-08-02 01:36:58 -0300

    alterado para usar banco de dev ou produção

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,8 +32,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    console.log('Emparedado');
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'https://lariflix-nine.herokuapp.com/categorias';
     fetch(URL)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
